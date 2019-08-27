930c680dd403a09de9aaab500a4d16c896d086c3
Steve Cirelli <scirelli+git@gmail.com>
2019-08-26 23:15:23 -0400

    improvements

--- clean.js
+++ clean.js
@@ -1,38 +1,31 @@
 #!/usr/bin/env node
 
-const fs = require('fs'),
-    child_process = require('child_process'),
-    exec = child_process.exec,
-    execSync = child_process.execSync,
-    path = require('path');
+const trav = require('./traverse'),
+    traverser = trav();
 
-var hashTable = {};
- 
 if(process.argv.length <= 2) {
-    console.log("Usage: " + __filename + " path/to/directory");
+    console.log('Usage: ' + __filename + ' path/to/directory');
     process.exit(-1);
 }
- 
-var argPath = process.argv[2];
- 
-fs.readdir(argPath, function(err, items) {
-    for (var i=0,p='',r=''; i<items.length; i++) {
-        p = path.join(argPath, items[i]);
 
-        try{
-	    if(fs.lstatSync(p).isDirectory()) continue;
-            r = execSync('md5sum "' + p + '"').toString();
-            process.stdout.write(r + '\n');
-	    r = r.split(' ')[0];
-            if(!hashTable[r]){
-                hashTable[r] = true;
-            }else{
-                execSync('rm -f "' + p + '"');
-                console.log('rm -f "' + p + '"');
-            }
-        }catch(e){
-            console.log(e);
+traverser.on('newHash', (path, hash)=> {
+    process.stdout.write('New \t' + path + '\t' + hash + '\n');
+}).on('dupHash', (path, hash)=> {
+    process.stdout.write('Dupe! \t' + path + '\t' + hash + '\n');
+}).on('error', (e)=>{
+    process.stderror.write(e);
+}).on('dirComplete', (argPath)=>{
+    process.stdout.write('### DIR Traverse Complete ###\t' + argPath);
+}).run(process.argv[2]).then((hashTable)=>{
+    let dupes = [];
+
+    Object.values(hashTable).forEach(v=> {
+        if(v.length > 1) {
+            dupes.push(v);
         }
+    });
 
-    }
+    //console.log(dupes.join('\n\n'));
+    console.log(dupes.length);
+    return dupes;
 });
