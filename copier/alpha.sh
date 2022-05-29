c5980ab4925184f5a6a9f8509932b0465b47a58c
scirelli <scirelli+git@gmail.com>
2022-05-29 08:47:19 -0400

    Made some changes to copier. It now accepts a src dir and destination dir. As well as a debug flag. This should all be made into a script that accepts flags

--- copier/alpha.sh
+++ copier/alpha.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
-debug=$1
+srcDir=${1:-'.'}
+dstDir=${2:-'/tmp'}
+debug=${3:-''}
+
+# Example
+#                 src               dst    debug
+# ./sortDir.sh ./DVDs_Not_Burned ./DVDs
 
 list=(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
 
 for item in ${list[*]}; do
-	mkdir ./$item
+	if [ -n "$debug" ]; then
+		echo mkdir -p "$dstDir/$item"
+	else
+		mkdir -p "$dstDir/$item"
+	fi
 done
 
 for item in ${list[*]}; do
 	if [ -n "$debug" ]; then
-		find . -maxdepth 1 -iname "$item*" -type f -exec echo "mv \"{}\" ./$item/" \;
+		set -x
+		find "$srcDir" -mindepth 1 -maxdepth 1 -iname "$item*" -type d -exec echo mv "\"{}\"" "\"$dstDir/$item/\"" \;
+		set +x
 	else
-		find . -maxdepth 1 -iname "$item*" -type f -exec mv "{}" ./$item/ \;
+		find "$srcDir" -mindepth 1 -maxdepth 1 -iname "$item*" -type d -exec mv "{}" "$dstDir/$item/" \;
 	fi
 done
